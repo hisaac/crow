f7ce3a0a6a77ce2aeac71586881a687c4b2ba5dd
Isaac Halvorson <hello@hisaac.net>
2017-01-02 12:35:21 -0600

    update database connection for heroku and mLab

--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var databaseUri = 'mongodb://localhost:27017/crow';
+var databaseUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/crow';
 var twitter = require('./modules/twitter.module');
 var db = require('./modules/db.module');
 require('dotenv').config({path: __dirname + '/.env'});
